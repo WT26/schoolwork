a6042ce461fbba113b6d2fbe58d19bc32bf38d04
Joel Alanko <joel.alanko@student.tut.fi>
2015-12-12 14:55:29 +0200

    lisatty split ja merkkien tarkastuksia

--- palautus-05/apufunktiot.cpp
+++ palautus-05/apufunktiot.cpp
@@ -14,28 +14,55 @@ void lue_muunnoskaaviot(){
 
         // Kaydaan lapi jokainen tiedoston rivi erikseen.
         while (getline (kaaviot, rivi)){
+            string kohdeyksikko;
+            string lahtoyksikko;
+            double suhde;
+            double lisattava{0};
 
+
+            if (tarkista_rivi(rivi)){
+                string valilyonniton = poista_valilyonnit(rivi);
+
+                vector<string> kaavio = split(valilyonniton, '=');
+
+
+            }
             // Kaydaan lapi jokainen rivilla oleva kirjain erikseen
             // ja tarkastetaan onko ne sopivia.
-            for(char& c : rivi) {
-                if (tarkista_rivin_vaihto(c) == false){
-                    if (onko_valilyonti(c) == true){
-                        break;
-                    }
-                    else {
-                        cout << rivi << endl;
-                    }
-                }
-                else {
-                    break;
-                }
-            }
+
         }
         kaaviot.close();
     }
     else {
         cout << "Virhe: Kaavio tiedostoa ei saatu luettua." << endl;
     }
+}
+
+
+// Vanha tuttu split.
+vector<string> split(const string& merkkijono, char erotinmerkki) {
+    vector<string> tulos;
+
+    string::size_type tutkittava_kohta{0};
+    string kentan_sisalto{""};
+
+    while ( tutkittava_kohta < merkkijono.length() ) {
+
+        if ( merkkijono.at(tutkittava_kohta) != erotinmerkki ) {
+            kentan_sisalto.push_back( merkkijono.at(tutkittava_kohta) );
+
+        } else {
+
+            tulos.push_back(kentan_sisalto);
+            kentan_sisalto = "";
+        }
+
+        ++tutkittava_kohta;
+    }
+
+    tulos.push_back(kentan_sisalto);
+
+    return tulos;
 }
 
 
@@ -57,3 +84,35 @@ bool onko_valilyonti(char c){
         return false;
     }
 }
+
+
+bool tarkista_rivi(string rivi){
+    for(char& c : rivi) {
+
+        // Tarkistaa onko selattava kirjain valilyonti tai
+        // kommentti (#) tai rivin vaihto. Jos jokin naista
+        // tulee ennen kirjainta, palautetaan false. Muulloin
+        // palautetaan true.
+        if (onko_valilyonti(c) == false){
+            if(tarkista_rivin_vaihto(c) == false){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
+
+
+string poista_tietty_merkki(string rivi, char poistettava_merkki){
+    string valilyonniton_rivi;
+
+    for(char& c : rivi) {
+        if(onko_valilyonti(c) == false){
+            valilyonniton_rivi += c;
+        }
+    }
+    return valilyonniton_rivi;
+}
+
--- palautus-05/apufunktiot.hh
+++ palautus-05/apufunktiot.hh
@@ -1,11 +1,20 @@
 #ifndef APUFUNKTIOT_HH
 #define APUFUNKTIOT_HH
 
+#include <string>
+#include <vector>
+
 using namespace std;
 
 void lue_muunnoskaaviot();
 
 bool tarkista_rivin_vaihto(char c);
 bool onko_valilyonti(char c);
+bool tarkista_rivi(string rivi);
+
+string poista_valilyonnit(string rivi);
+
+vector<string> split(const string& merkkijono, char erotinmerkki);
+
 
 #endif // APUFUNKTIOT_HH
